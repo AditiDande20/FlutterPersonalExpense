395052db034ab660ffa1b2727b93f5f27cf2ef01
Aditi Dande <aditidande20@gmail.com>
2024-03-04 19:49:52 +0530

    bug fixes

--- personal_expense_app/lib/main.dart
+++ personal_expense_app/lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'PersonalExpenseApp',
       theme: ThemeData(primarySwatch: Colors.yellow, fontFamily: 'Quicksand'),
       home: MyHomePage(title: 'PersonalExpenseApp'),
